5427cd1611dad041917816f861db0318f14a0b3a
liyu <liyudesta22@gmail.com>
2024-07-03 13:12:02 +0300

    Caesar Cipher encryption and decryption

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -3,9 +3,9 @@ def encrypt(PlainText, shift_value):
     for i in PlainText:
         if i.isalpha():  # Check if the character is a letter
             if i.islower():
-                Cy_text += chr((ord(i) - ord('a') + shift_value) % 26 + ord('a')) # shift each character by the value of shift_value
+                Cy_text += chr((ord(i) - ord('a') + shift_value) % 26 + ord('a')) # shift each character by the value of shift_value for small letters
             elif i.isupper():
-                Cy_text += chr((ord(i) - ord('A') + shift_value) % 26 + ord('A'))
+                Cy_text += chr((ord(i) - ord('A') + shift_value) % 26 + ord('A'))# shift each character by the value of shift_value for capital letters
         else:
             Cy_text += i  # Non-alphabet characters remain unchanged
     return Cy_text
@@ -15,9 +15,9 @@ def decrypt(cipherText, shift_value):
     for i in cipherText:
         if i.isalpha():
             if i.islower():
-                Pl_Text += chr((ord(i) - ord('a') - shift_value) % 26 + ord('a'))
+                Pl_Text += chr((ord(i) - ord('a') - shift_value) % 26 + ord('a')) # shift each character back by the value of shift_value for small letters
             elif i.isupper():
-                Pl_Text += chr((ord(i) - ord('A') - shift_value) % 26 + ord('A'))
+                Pl_Text += chr((ord(i) - ord('A') - shift_value) % 26 + ord('A'))# shift each character back by the value of shift_value for capital letters
         else:
             Pl_Text += i
     return Pl_Text
